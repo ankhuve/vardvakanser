464b49b34c5f8299d93ab671f6cee96e7d8debfe
Erik Forsberg <erik.c.forsberg@gmail.com>
2017-02-26 16:22:57 +0100

    Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,16 @@
                 'csrfToken' => csrf_token(),
         ]); ?>
     </script>
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-83188287-3', 'auto');
+        ga('send', 'pageview');
+    </script>
 </head>
 <body>
 <nav class="navbar navbar-custom navbar-static-top">
