9b23d29e55bbd1e2960ea7c2ffc90ec63515f325
Erik Forsberg <erik.c.forsberg@gmail.com>
2017-03-11 13:46:40 +0100

    Versioning css/js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
     <title>@yield('title', config('app.name', 'Vårdvakanser'))</title>
 
     <!-- Styles -->
-    <link href="{{ elixir('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 
